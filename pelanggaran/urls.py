85c62c85dcfd81be6d7357808215940b7dbb92fe
raysasitiaisyah <119478940+raysasitiaisyah@users.noreply.github.com>
2023-01-15 16:06:55 +0700

    Add files via upload

--- pelanggaran/urls.py
+++ pelanggaran/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from pelanggaransiswa.views import data
+from pelanggaransiswa.views import *
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data/', data),
+    path('data/', data, name='data'),
+    path('tambah/', tambah_siswa, name='tambah_siswa'),
+    path('data/hapus/<int:id>', hapus_siswa, name='hapus_siswa'),
+    path('masuk/', LoginView.as_view(), name='masuk'),
+    path('keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
+
 ]
