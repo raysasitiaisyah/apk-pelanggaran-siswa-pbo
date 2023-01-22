c7051abc061df286e47f19605e581da8764b6ea8
raysasitiaisyah <119478940+raysasitiaisyah@users.noreply.github.com>
2023-01-22 16:36:41 +0700

    Add files via upload

--- pelanggaran/urls.py
+++ pelanggaran/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('data/', data, name='data'),
     path('tambah/', tambah_siswa, name='tambah_siswa'),
-    path('data/hapus/<int:id>', hapus_siswa, name='hapus_siswa'),
+    path('data/hapus/<int:id_data>', hapus_siswa, name='hapus_siswa'),
     path('masuk/', LoginView.as_view(), name='masuk'),
     path('keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
-
+    path('data/<int:iddata>/', rincian_pelanggar),
 ]
